b09d971093ffcfa85cd405bcb63f14dc5cc4267b
Khanhbroo <khanhmathias532211@gmail.com>
2025-06-26 06:00:15 +0700

    feat: implement FeaturesSection layout (v0.0.6)

--- src/components/landing/index.ts
+++ src/components/landing/index.ts
@@ -1,6 +1,6 @@
 export { default as HeroSection } from "./hero-section";
 export { default as WeDoSection } from "./we-do-section";
-// export { default as FeaturesSection } from "./features-section";
+export { default as FeaturesSection } from "./features-section";
 // export { default as FrameworkSection } from "./framework-section";
 // export { default as TokenomicsSection } from "./tokenomics-section";
 // export { default as MetricSection } from "./metric-section";
