421d88911f47e158556a5612f25d134ba12f8c15
Khanhbroo <khanhmathias532211@gmail.com>
2025-06-26 06:08:12 +0700

    feat: implement MetricSection layout (v0.0.8)

--- src/components/card/index.ts
+++ src/components/card/index.ts
@@ -3,3 +3,4 @@ export { default as HeadCard } from "./head-card";
 export { default as TechnologyPartnerCard } from "./tehnology-partners-card";
 export { default as VerticalCard } from "./vertical-card";
 export { default as CardWithIcon } from "./card-with-icon";
+export { default as MetricCard } from "./metric-card";
--- src/components/landing/index.ts
+++ src/components/landing/index.ts
@@ -3,5 +3,5 @@ export { default as WeDoSection } from "./we-do-section";
 export { default as FeaturesSection } from "./features-section";
 export { default as FrameworkSection } from "./framework-section";
 // export { default as TokenomicsSection } from "./tokenomics-section";
-// export { default as MetricSection } from "./metric-section";
+export { default as MetricSection } from "./metrics-section";
 // export { default as FAQSection } from "./faq-section";
