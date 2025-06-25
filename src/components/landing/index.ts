3cbecc0acb0f8ed377c8800f5924ca74a64c87e3
Khanhbroo <khanhmathias532211@gmail.com>
2025-06-26 06:10:59 +0700

    feat: implement FAQSection layout (v0.0.9)

--- src/components/landing/index.ts
+++ src/components/landing/index.ts
@@ -4,4 +4,4 @@ export { default as FeaturesSection } from "./features-section";
 export { default as FrameworkSection } from "./framework-section";
 // export { default as TokenomicsSection } from "./tokenomics-section";
 export { default as MetricSection } from "./metrics-section";
-// export { default as FAQSection } from "./faq-section";
+export { default as FAQSection } from "./faq-section";
